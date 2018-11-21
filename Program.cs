a922bfbf59a1168063a8e6e2b9d391796be59920
Hyunseok Song <38483113+Microsongs@users.noreply.github.com>
2018-11-21 16:33:13 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi
+{
+    class Program
+    {
+        static int count = 0;
+        static void Move(int N,int Start,int Def)
+        {
+            Console.WriteLine(N + "번째 원반이 " + (char)Start+ "에서" + (char)Def+ "으로 이동");
+        }
+        static void hanoi(int N,int Start,int Buff,int Def)
+        {
+            count++;
+            if (N == 1)
+            {
+                Move(N, Start, Def);
+            }
+            else
+            {
+                hanoi(N - 1, Start, Def, Buff);
+                Move(N, Start, Def);
+                hanoi(N - 1, Buff, Start, Def);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int N;  //원반
+            Console.Write("원반 개수 입력 : ");
+            N = int.Parse(Console.ReadLine());
+            hanoi(N, (int)'A', (int)'B', (int)'C');
+
+            Console.WriteLine("총 이동 횟수 : " + count);
+        }
+    }
+}
